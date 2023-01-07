941cb17619b1133f91de328bebfdc397c3a3f595
Endurance Orumgbe <eorumgbe@yahoo.com>
2023-01-06 17:09:46 -0800

    static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if parameter is uppercase
+ * @c: input character
+ * Return: 1 if it's an uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
