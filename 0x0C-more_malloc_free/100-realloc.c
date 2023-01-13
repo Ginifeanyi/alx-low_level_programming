67e7caf8237c0dbad52f197e18ca941679188f86
Endurance Orumgbe <eorumgbe@yahoo.com>
2023-01-13 03:02:45 -0800

    A function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,9 +3,9 @@
 /**
  * _realloc - reallocate a memory block using malloc and free
  *
- * @ptr: pointer to previous memory
- * @old_size: old memory array size
- * @new_size: new memory array size
+ * @ptr: pointer to previous memory.
+ * @old_size: old memory array size.
+ * @new_size: new memory array size.
  *
  * Return: pointer to new memory
  *         NULL if it fails
